89c7ec7d9a93fa59d73d86dfadea624ac3167c07
Khath Khemanorak <khemanorak.k@codium.co>
2017-02-14 17:40:22 +0700

    update setup.py and add setup.cfg

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
 setup(
-    name='django-common-boxset',
+    name='django-model-controller',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
