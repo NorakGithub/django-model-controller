9c7f9922c8413e0c3ae00f7045189e3d10bf12c1
Khemanorak Khath <khemanorak.k@codium.co>
2018-08-13 15:59:54 +0700

    Added RandomFileName class for file storing

--- model_controller/utils.py
+++ model_controller/utils.py
@@ -1,1 +1,22 @@
+import os
+import time
+from uuid import uuid4
+
+from django.utils.deconstruct import deconstructible
+
 EXCLUDE_MODEL_CONTROLLER_FIELDS = ('created_at', 'updated_at', 'created_user', 'updated_user')
+
+
+@deconstructible
+class RandomFileName(object):
+    def __init__(self, path):
+        # allow to customize path in __call__
+        self.path = path
+
+    def __call__(self, _, filename):
+        extension = os.path.splitext(filename)[1]
+        return os.path.join(self.path, "%s-%s%s") % (
+            time.strftime("%Y%m%d-%H%M%S"),
+            uuid4(),
+            extension
+        )
