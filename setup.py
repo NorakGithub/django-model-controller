d825474fe0044724f0359493837203005894b3f2
Khemanorak Khath <khath.khemanorak@gmail.com>
2020-04-24 19:22:25 +0700

    Change version to 0.2.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-model-controller',
-    version='{tag}',
+    version='0.2.5',
     packages=find_packages(),
     include_package_data=True,
     license='BSD',  # example license
@@ -22,9 +22,6 @@ setup(
     author='Khemanorak Khath',
     author_email='khath.khemanorak@google.com',
     keywords='django model controller tracking',
-    setup_requires=[
-        'setuptools-git-version'
-    ],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
