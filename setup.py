30646a060f94121c1e48639f61a13257e8ebbdb9
Khemanorak Khath <khath.khemanorak@gmail.com>
2020-05-26 11:14:14 +0700

    Update version to 0.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-model-controller',
-    version='0.2.5',
+    version='0.3.0',
     packages=find_packages(),
     include_package_data=True,
     license='BSD',  # example license
