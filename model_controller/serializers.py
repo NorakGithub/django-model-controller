3da0be58149f182d0d68f2f61395867a28b68569
Khemanorak Khath <khath.khemanorak@gmail.com>
2020-05-26 10:49:16 +0700

    Added serializer for model controller without foreign key

--- model_controller/serializers.py
+++ model_controller/serializers.py
@@ -23,3 +23,21 @@ class ModelControllerSerializer(serializers.ModelSerializer):
         validated_data['updated_user'] = user
 
         return super(ModelControllerSerializer, self).update(instance, validated_data)
+
+
+class ModelControllerWithoutForeignKeySerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['created_user_id'] = user.id
+        return super(
+            ModelControllerWithoutForeignKeySerializer,
+            self
+        ).create(validated_data)
+
+    def update(self, instance, validated_data):
+        user = self.context['request'].user
+        validated_data['updated_user_id'] = user.id
+        return super(
+            ModelControllerWithoutForeignKeySerializer,
+            self
+        ).update(instance, validated_data)
